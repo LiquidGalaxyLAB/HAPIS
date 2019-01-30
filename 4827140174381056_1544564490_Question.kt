ed66dbbfba117d9fbb2d23d20f69c73afe16e417
Claudia Diosan <36296848+ClaudiaMihaelaDiosan@users.noreply.github.com>
2019-01-30 10:53:34 +0100

    Delete 4827140174381056_1544564490_Question.kt

--- 4827140174381056_1544564490_Question.kt
+++
@@ -1,8 +0,0 @@
-package com.example.krzysztof.hapis_questionare
-
-class Question(questionId: Int,questionName: String, questionBody: String, answers: Array<String>) {
-     var QuestionId = questionId
-     var QuestionName: String = questionName
-     var QuestionBody: String = questionBody
-     var Answers: Array<String> = answers
-}
